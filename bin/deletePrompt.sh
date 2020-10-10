6337a3c0be29e199fe7355223ba9fd811848dbdf
RolandWarburton <warburtonroland@gmail.com>
2020-10-10 17:41:05 +1100

    :wrench: Improve the delete prompter

--- bin/deletePrompt.sh
+++ bin/deletePrompt.sh
@@ -6,8 +6,18 @@ for var in "$@"; do
 	FILELIST+="\n$(basename "$var")"
 done
 
-if zenity --question --width 250 --text="Delete:$FILELIST?"; then
-	for var in "$@"; do
-		trash-put "$var"
-	done
+# Check the first path to see if its on the home path
+if [ "$1" == *"$HOME"* ]; then
+	# Its a file we can delete by promting
+	if zenity --question --width 250 --text="Delete:$FILELIST?"; then
+		for var in "$@"; do
+			trash-put "$var"
+		done
+	fi
+else
+	if zenity --question --width 250 --text="Delete PERMANENTLY:$FILELIST?"; then
+		for var in "$@"; do
+			rm -rf "$var"
+		done
+	fi
 fi
