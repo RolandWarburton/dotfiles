dd1bde39b3132b002653620ebe7b1c0bf8bb78d3
RolandWarburton <warburtonroland@gmail.com>
2021-05-07 23:01:51 +1000

    :construction: Update macroboard to write config

--- bin/macroboard.py
+++ bin/macroboard.py
@@ -9,6 +9,9 @@ import sys
 import os
 import apt
 import getpass
+from pathlib import Path
+import subprocess
+import time
 
 
 def question(q): return input(q).lower().strip()[0] == "y" or question(q)
@@ -43,6 +46,38 @@ def preFlight(applications):
         return False
 
 
+# https://stackoverflow.com/questions/36596354/how-to-get-exit-code-from-subprocess-popen/36596554
+def execCommand(params):
+    # Create a process
+    p = subprocess.Popen(params,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.STDOUT)
+
+    try:
+        # Filter stdout
+        for line in iter(p.stdout.readline, ''):
+            # Get everything out of the buffer and onto the screen
+            # When we execute print statements output will be written to buffer (flushed)
+            #   and we will see the output on screen when buffer get flushed (cleared).
+            #   By default buffer will be flushed when program exits.
+            sys.stdout.flush()
+            # Print the flushed buffer or whatever i don't really understand how this works
+            print(">>> " + line.rstrip())
+            sys.stdout.flush()
+    except:
+        sys.stdout.flush()
+
+    # Wait until process terminates (without using p.wait())
+    while p.poll() is None:
+        # Process hasn't exited yet, let's wait some
+        time.sleep(0.5)
+
+    # Get return code from process
+    return_code = p.returncode
+
+    print('RETURN CODE', return_code)
+
+
 def main():
     # check required packages are installed
     if not preFlight(["xinput", "jumpapp"]):
@@ -55,21 +90,23 @@ def main():
     disableDevice()
 
     try:
+        # If we passed -y then don't bother asking if we want to generate a config
         if sys.argv[1] == "-y":
             generateConfig = True
     except:
         generateConfig = question("write config to /etc/actkbd ")
 
     if generateConfig:
-        print(f"writing config")
-        print(sys.argv[0])
-        abspath = os.path.abspath(sys.argv[0])
-        pathTemp = os.path.split(abspath)[0]
-        path = os.path.join(pathTemp, "../actkbd.conf")
-        print(path)
-        # cmd = "sudo cp ../actkbd.conf /etc/actkbd.conf"
-        # res = os.popen(cmd).read()
-        # print(res)
+        # Write the config
+        print("writing config")
+
+        # Am i retarded or is this actually the right way of getting the parent directory?
+        actkbdPath = Path(sys.argv[0]).parent.absolute().parent.absolute()
+
+        # Run this command
+        # cmd = f"sudo cp {actkbdPath}/actkbd.conf /etc/actkbd.conf"
+        cmd = ["sudo", "cp", f"{actkbdPath}/actkbd.conf", "/etc/actkbd.conf"]
+        execCommand(cmd)
 
 
 main()
