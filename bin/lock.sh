ea78a6c6cd8fda03f3e4a318e3825692eb8ef669
RolandWarburton <warburtonroland@gmail.com>
2021-05-13 15:10:31 +1000

    Add blur lock screen script

---
+++ bin/lock.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# https://www.andreafortuna.org/2020/04/09/i3-how-to-make-a-pretty-lock-screen-with-a-four-lines-of-bash-script/
+
+# create a temp file
+img=$(mktemp /tmp/XXXXXXXX.png)
+
+# Take a screenshot of current desktop
+import -window root $img
+
+# Pixelate the screenshot
+convert $img -scale 10% -scale 1000% $img
+
+# Run i3lock with custom background
+i3lock -i $img
+
+# Remove the tmp file
+rm $img
