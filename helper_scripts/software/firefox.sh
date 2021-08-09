942ff8bad989fbcb6a77ec9d2574dccd029e680a
RolandWarburton <warburtonroland@gmail.com>
2021-08-09 12:45:40 +1000

    Improve the FF install scrip

--- helper_scripts/software/firefox.sh
+++ helper_scripts/software/firefox.sh
@@ -2,6 +2,21 @@ if [ -d "/opt/firefox" ]; then
 	sudo mv /opt/firefox /opt/firefox.bak
 fi
 
+# Remove FF tmp donwload location if it exists
+if [ -d "/tmp/firefox" ]
+then
+	echo "Removing old FF tmp directory. This wont impact your FF data"
+	sudo rm -rf /tmp/firefox
+fi
+
+# Back up the existing FF install
+if [ -d "/opt/firefox" ]
+then
+	echo "Backing up original FF install"
+	sudo rm -rf /opt/firefox.bak
+	sudo cp -p /opt/firefox /opt/firefox.bak
+fi
+
 mkdir -p /tmp/firefox # create a temp location to store the downloaded binary
 wget -O /tmp/firefox/FirefoxSetup.tar.bz2 "https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US"
 sudo tar -xf /tmp/firefox/FirefoxSetup.tar.bz2 --directory /opt # extract firefox to /opt
