70456c939614c839c4851116ff0c5b6a5f7c6e98
RolandWarburton <warburtonroland@gmail.com>
2022-03-18 15:54:11 +1100

    Edit vsc theme again

--- vscode/settings/theme.js
+++ vscode/settings/theme.js
@@ -3,7 +3,6 @@ export const theme = {
   "workbench.colorCustomizations": {
     "editor.selectionHighlightBorder": "#00ff2a",
     "editor.lineHighlightBackground": "#154E18",
-    "editorLineNumber.activeForeground": "#000000",
     "editorCursor.foreground": "#ff0000",
     "list.focusBackground": "#ff000043",
     "statusBar.noFolderBackground": "#af2828",
