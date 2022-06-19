f7e1ca417b4c27ab99350a3beafdfe2b07b9ff7a
RolandWarburton <warburtonroland@gmail.com>
2022-06-19 22:08:14 +1000

    add html to lsp

--- nvim/lua/lsp/lsp.lua
+++ nvim/lua/lsp/lsp.lua
@@ -41,16 +41,35 @@ local on_attach = function(client, bufnr)
   vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
 end
 
+-- this hooks nvim-csp in with nvim-lsp
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
 -- Typescript
 -- npm i -g typescript typescript-language-server eslint prettier
 -- https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#tsserver
 -- https://github.com/typescript-language-server/typescript-language-server
 require 'lspconfig'.tsserver.setup {
+  capabilities = capabilities,
   on_attach = on_attach,
   cmd = { "typescript-language-server", "--stdio" },
   filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
   init_options = { hostInfo = "neovim" },
   root_dir = root_pattern("package.json", "tsconfig.json", "jsconfig.json", ".git")
+}
+
+-- HTML
+-- npm i -g vscode-langservers-extracted
+require'lspconfig'.html.setup {
+  capabilities = capabilities,
+  filetypes = { "html" },
+  init_options = {
+    configurationSection = { "html", "css", "javascript" },
+    embeddedLanguages = {
+      css = true,
+      javascript = true
+    },
+    provideFormatter = true
+  }
 }
 
 -- USER = vim.fn/expand('$USER')
