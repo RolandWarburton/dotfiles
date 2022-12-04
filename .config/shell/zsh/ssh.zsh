1605acad352a56fdbff0db7f96a13b01f1bfde3d
RolandWarburton <warburtonroland@gmail.com>
2022-12-04 22:42:29 +1100

    Update zsh ssh.zsh to export SSH_AUTH_SOCK if it exists

--- .config/shell/zsh/ssh.zsh
+++ .config/shell/zsh/ssh.zsh
@@ -1,11 +1,8 @@
-# This answer looks good but doesnt seem to work for me
-# https://unix.stackexchange.com/questions/90853/how-can-i-run-ssh-add-automatically-without-a-password-prompt
+# Refer to /usr/lib/systemd/user/ssh-agent.service
+# which creates this socket
 
-# start the ssh agent because it always seems to wanna kill itself
-eval `ssh-agent -s` > /dev/null
-
-# Add my nopass key if i have it on this system
-if [ -f "$HOME/.ssh/id_nopass_rsa" ]; then
-    ssh-add -q  ~/.ssh/id_nopass_rsa
+# check if the ssh-agent.socket file exists in the user runtime directory
+if [[ -e "$XDG_RUNTIME_DIR/ssh-agent.socket" ]]; then
+  export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
 fi
 
