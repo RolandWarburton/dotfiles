7f340958426b3574f880947b41eb37c4640e9917
RolandWarburton <warburtonroland@gmail.com>
2022-12-05 08:43:05 +1100

    Add tmux toggle prefix script

---
+++ .local/scripts/tmux-toggle-prefix.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Run the `tmux show-environment` command and store the output in a variable
+output=$(tmux show-environment)
+
+# the <<< operator is used to redirect a string (known as a here string)
+# grep quietly (-q) gets a return code and searches for the string in quotes "TMUX_PREFIX=a".
+# grep sources its text to grep for by reading from the RHS of <<<
+# the <<< operator is used to redirect the contents of the output variable as standard input to
+# the grep command.
+if grep -q "TMUX_PREFIX=a" <<< "$output"; then
+  tmux unbind C-a
+  tmux set -g prefix C-b
+  tmux set-environment TMUX_PREFIX b;
+  tmux display-message "Now using B prefix";
+else
+  tmux set-environment TMUX_PREFIX a;
+  tmux display-message "Now using A prefix";
+  tmux unbind C-b
+  tmux set -g prefix C-a
+fi
