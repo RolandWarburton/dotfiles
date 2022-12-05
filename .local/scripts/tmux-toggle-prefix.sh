2cded96a44dbf45b3698f98ed790646831b8db12
RolandWarburton <warburtonroland@gmail.com>
2022-12-05 21:40:46 +1100

    Swap IF statement around - assume C-a is set by default in tmux

--- .local/scripts/tmux-toggle-prefix.sh
+++ .local/scripts/tmux-toggle-prefix.sh
@@ -8,14 +8,14 @@ output=$(tmux show-environment)
 # grep sources its text to grep for by reading from the RHS of <<<
 # the <<< operator is used to redirect the contents of the output variable as standard input to
 # the grep command.
-if grep -q "TMUX_PREFIX=a" <<< "$output"; then
-  tmux unbind C-a
-  tmux set -g prefix C-b
-  tmux set-environment TMUX_PREFIX b;
-  tmux display-message "Now using B prefix";
-else
+if grep -q "TMUX_PREFIX=b" <<< "$output"; then
   tmux set-environment TMUX_PREFIX a;
   tmux display-message "Now using A prefix";
   tmux unbind C-b
   tmux set -g prefix C-a
+else
+  tmux unbind C-a
+  tmux set -g prefix C-b
+  tmux set-environment TMUX_PREFIX b;
+  tmux display-message "Now using B prefix";
 fi
