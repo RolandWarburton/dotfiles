6dc82739049839565fded2932834b492a29d6096
RolandWarburton <warburtonroland@gmail.com>
2022-12-20 15:05:35 +1100

    Add hibernate alias

--- .config/shell/zsh/aliases.zsh
+++ .config/shell/zsh/aliases.zsh
@@ -32,6 +32,7 @@ alias dockerbuild='docker-compose build'
 alias viviInspect='cat $(cat ~/.vivi_tools.json | jq -r ".tmp.inspectFile") | fx'
 alias vim='nvim'
 alias prunebranches='git remote prune origin'
+alias hibernate='systemctl hibernate'
 
 # If LSD is installed then use those
 if which lsd >/dev/null
