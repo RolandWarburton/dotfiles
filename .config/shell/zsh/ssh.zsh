6b95bdfd29d80ca05f4fcac61b9143c0539bd1fc
RolandWarburton <warburtonroland@gmail.com>
2024-01-23 08:27:07 +1100

    Add comment documentation in ssh.zsh

--- .config/shell/zsh/ssh.zsh
+++ .config/shell/zsh/ssh.zsh
@@ -1,3 +1,11 @@
+# The purpose of this is to try and have just one ssh-agent running at a time
+# when called it will check eval it the existing agent, then add my github key.
+#
+# when developing you can find unnecessary processes using `ps -ef | grep ssh-agent `
+#
+# IMPORTANT
+# The ssh-agent will need to be instantiated somewhere else. I suggest in your WM/DE init script (sway, xfce, etc)
+
 ssh_agent_file="$HOME/.ssh-agent"
 if [ -f $HOME/.ssh/id_github ] && [ -f "$ssh_agent_file" ]; then
   eval "$(cat "$ssh_agent_file")" 2>&1
