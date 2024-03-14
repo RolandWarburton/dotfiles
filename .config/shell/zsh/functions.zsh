b00291128e64328a7edee2f6d79bfc079555c389
RolandWarburton <warburtonroland@gmail.com>
2024-03-14 02:52:46 +0000

    Add dockersh function

--- .config/shell/zsh/functions.zsh
+++ .config/shell/zsh/functions.zsh
@@ -1,3 +1,11 @@
 count_lines() {
   find "$1" -type f -exec cat {} + | wc -l
+dockersh() {
+  if [ $# -eq 0 ]; then
+    echo "Usage: dockersh <container_name>"
+    return 1
+  fi
+
+  local container_name="$1"
+  docker exec -it "$container_name" /bin/bash
 }
