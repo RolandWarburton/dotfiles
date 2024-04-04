a3be1003bef75af5842d02bee355559830a31b7a
RolandWarburton <warburtonroland@gmail.com>
2024-04-04 14:47:42 +1100

    Add zsh-select-with-lf.plugin.zsh plugin

--- .config/shell/zsh/plugins.zsh
+++ .config/shell/zsh/plugins.zsh
@@ -11,6 +11,13 @@ zgen load "zsh-users/zsh-syntax-highlighting"
 # interative git commands
 zgen load 'wfxr/forgit'
 
+export ZSH_SELECT_WITH_LF_DIR_BIND="^g"
+export ZSH_SELECT_WITH_LF_FILE_BIND="^p"
+LF_SELECT_PLUGIN_PATH="$HOME/.zsh/plugins/zsh-select-with-lf.plugin.zsh"
+if [ -f "$LF_SELECT_PLUGIN_PATH" ]; then
+  source "$LF_SELECT_PLUGIN_PATH"
+fi
+
 LFCD="$HOME/.config/lf/lfcd.sh"
 if [ -f "$LFCD" ]; then
   source "$LFCD"
---
+++ .config/shell/zsh/plugins/zsh-select-with-lf.plugin.zsh
@@ -0,0 +1,44 @@
+#
+# ======================================================================
+# copyright belongs to https://github.com/chmouel/zsh-select-with-lf
+# ======================================================================
+#
+
+(( ! $+commands[lf] )) && return
+
+(( ! ${+ZSH_SELECT_WITH_LF_FILE_BIND} )) && typeset -g ZSH_SELECT_WITH_LF_FILE_BIND='^x^l'
+(( ! ${+ZSH_SELECT_WITH_LF_DIR_BIND} )) && typeset -g ZSH_SELECT_WITH_LF_DIR_BIND='^xl'
+
+__select_with_lf_file() {
+    action=${1:-print-selection}
+    local current=${LBUFFER##* }
+    local initial_directory=${PWD}
+    [[ -n ${current} ]] && {
+        current=${current%%[[:space:]]#}
+        [[ ${current} == '~'* ]] && current=${HOME}${current#'~'}
+    }
+    if [[ -d ${current} ]]; then
+        initial_directory=${current}
+    elif [[ -f ${current} ]]; then
+        initial_directory=${current%/*}
+    elif [[ -d ${current%/*} ]]; then
+        initial_directory=${current%/*}
+    fi
+    selection="$(command lf -${action} ${initial_directory})"
+    [[ -z ${selection} ]] && return 1
+    [[ ${selection} == $HOME/* && ${selection} != *" "* ]] && selection="~${selection#$HOME}"
+    [[ ${selection} == *" "* ]] && selection="\"${selection}\""
+    [[ -n ${current} ]] && LBUFFER="${LBUFFER% *} "
+    LBUFFER="$LBUFFER$selection"
+}
+
+__select_with_lf_dir() {
+    __select_with_lf_file print-last-dir
+}
+
+autoload -U __select_with_lf_file __select_with_lf_dir
+zle -N __select_with_lf_file
+zle -N __select_with_lf_dir
+
+bindkey $ZSH_SELECT_WITH_LF_FILE_BIND __select_with_lf_file
+bindkey $ZSH_SELECT_WITH_LF_DIR_BIND __select_with_lf_dir
