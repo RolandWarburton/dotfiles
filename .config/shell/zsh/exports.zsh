750851aed33be8da91f3132ca3b45baee90fb1ea
RolandWarburton <warburtonroland@gmail.com>
2024-05-21 21:56:22 +1000

    Add exports for user defined lua modules

--- .config/shell/zsh/exports.zsh
+++ .config/shell/zsh/exports.zsh
@@ -4,6 +4,12 @@ export PATH=$PATH:$HOME/.local/bin
 export PATH=$PATH:$HOME/.local/scripts
 export XDG_CONFIG_HOME=$HOME/.config
 
+# add custom lua modules if its exists
+if command -v luarocks > /dev/null 2>&1 && [ -d "$HOME/.local/luarocks/5.1" ]; then
+  eval $(luarocks path)
+  export LUA_PATH="$LUA_PATH;$HOME/.local/luarocks/5.1/?/init.lua"
+fi
+
 # export default editor
 export EDITOR=nvim
 
---
+++ .local/luarocks/5.1/example-module/init.lua
@@ -0,0 +1,7 @@
+local M = {}
+
+M.sayHello = function()
+  print('hello')
+end
+
+return M
