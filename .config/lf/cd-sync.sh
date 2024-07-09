a1af955ecf623c78ef23cb2538044a80d25b045b
RolandWarburton <warburtonroland@gmail.com>
2024-07-09 17:49:52 +1000

    Move cd-sync to its own script

---
+++ .config/lf/cd-sync.sh
@@ -0,0 +1,26 @@
+# Synchronizes the current lf folder with the last tmux pane
+
+
+if [ ! -f "$HOME/.tmux-pane-id" ]; then
+  echo "no tmux pane history"
+  return 0
+fi
+# get the current window
+current_window=$(tmux display-message -p '#I')
+# get the last pane index
+last_pane=$(tail -n 2 ~/.tmux-pane-id | head -n 1)
+if [ -z "$last_pane" ]; then
+  echo "last_pane not found"
+  return 0
+fi
+pane_pid=$(tmux list-panes -t $current_window -F '#{pane_index} #{pane_pid}' | grep "$last_pane " | cut -d ' ' -f2 )
+if [ -z "$pane_pid" ]; then
+  echo "pane_pid not found"
+  return 0
+fi
+# check we are not running anything in this pane
+if $(pstree -p "$pane_pid" | grep -qE '^zsh\([0-9]+\)$'); then
+  tmux send-keys -t "$last_pane" "cd $PWD" C-m
+  return 0
+fi
+echo "refusing to open in $last_pane"
