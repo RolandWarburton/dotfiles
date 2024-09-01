e0d4562cbe3ae91f950e8c79870505dd2a31dcac
RolandWarburton <warburtonroland@gmail.com>
2024-09-02 08:28:31 +1000

    Better handling of multiple SSH keys

--- .config/shell/zsh/ssh.zsh
+++ .config/shell/zsh/ssh.zsh
@@ -6,13 +6,25 @@
 # IMPORTANT
 # The ssh-agent will need to be instantiated somewhere else. I suggest in your WM/DE init script (sway, xfce, etc)
 
+add_keys() {
+  # Define an array of SSH key paths
+  # multiple keys can be specified between the brackets with a space
+  # (key1 key2 key3)
+  local keys=(~/.ssh/id_github)
+  for key in "${keys[@]}"; do
+    if [ -f "$key" ]; then
+      ssh-add "$key" >/dev/null 2>&1
+    fi
+  done
+}
+
 ssh_agent_file="$HOME/.ssh-agent"
 if [ -f $HOME/.ssh/id_github ] && [ -f "$ssh_agent_file" ]; then
   eval "$(cat "$ssh_agent_file")" > /dev/null 2>&1
-  ssh-add ~/.ssh/id_github >/dev/null 2>&1
+  add_keys
 else
   echo "Writing SSH agent information to $HOME/.ssh-agent"
   ssh-agent > $HOME/.ssh-agent
   eval "$(cat "$ssh_agent_file")" 2>&1
-  ssh-add ~/.ssh/id_github >/dev/null 2>&1
+  add_keys
 fi
