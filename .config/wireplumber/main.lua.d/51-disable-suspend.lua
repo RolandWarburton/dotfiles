184796861df496862c52a0bf7c88f182ec78efae
RolandWarburton <warburtonroland@gmail.com>
2026-05-21 22:54:18 +1000

    Ring the bell when claude-notify is run

---
+++ .config/wireplumber/main.lua.d/51-disable-suspend.lua
@@ -0,0 +1,10 @@
+table.insert(alsa_monitor.rules, {
+  matches = {
+    {
+      { "node.name", "matches", "alsa_output.*" },
+    },
+  },
+  apply_properties = {
+    ["session.suspend-timeout-seconds"] = 0,
+  },
+})
